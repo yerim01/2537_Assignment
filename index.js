7abb5a064869319b6f0992a0f2dadf62590b80e0
Yerim Moon <myr9901@gmail.com>
2023-04-30 22:07:57 -0700

    updated index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
-const port = process.env.PORT || 3030;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
